95b67fe9954abe1aa920915e3fd6d70af3f2bd85
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-04 19:06:28 -0300

    feat(users/infra/grpc): create grpc infrastructure
    - create user proto file
    - create controller for grpc communction

---
+++ src/users/infra/grpc/server/controller/User.grpc-server.controller.ts
@@ -0,0 +1,16 @@
+import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+
+import type { InputCreateUserDto } from 'src/users/dto/CreateUser.dto';
+
+import { UserService } from 'src/users/user.service';
+
+@Controller()
+export class UserGrpcServerController {
+  constructor(private readonly userService: UserService) {}
+
+  @GrpcMethod('UserService')
+  async createUser(data: InputCreateUserDto) {
+    return this.userService.create(data);
+  }
+}
