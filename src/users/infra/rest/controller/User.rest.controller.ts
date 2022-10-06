ff2796b2249a528d85efd51153b7debc039e7b2e
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-06 16:06:31 -0300

    feat(users/infra/rest): create interceptor to transforme responses
    - use interceptor on create user method

--- src/users/infra/rest/controller/User.rest.controller.ts
+++ src/users/infra/rest/controller/User.rest.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseFilters } from '@nestjs/common';
+import { Body, Controller, Post, UseFilters, UseInterceptors } from '@nestjs/common';
 
 import type {
   InputCreateUserDto,
@@ -7,6 +7,7 @@ import type {
 
 import { UserService } from '@users/user.service';
 import { ExceptionRestFilter } from '../filter/ExceptionRest.filter';
+import { ParseHalJsonInterceptor } from '../interceptor/Parse.hal-json.interceptor';
 
 @Controller('/users')
 @UseFilters(new ExceptionRestFilter())
@@ -14,6 +15,7 @@ export class UserRestController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @UseInterceptors(new ParseHalJsonInterceptor<OutputCreateUserDto>())
   async create(@Body() data: InputCreateUserDto): Promise<OutputCreateUserDto> {
     const { id, username } = await this.userService.create(data);
     return { id, username };
---
+++ src/users/infra/rest/interceptor/Parse.hal-json.interceptor.ts
@@ -0,0 +1,28 @@
+import { NestInterceptor, CallHandler, ExecutionContext } from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+
+import type { RestResponseCreateUser } from '@shared/infra/rest/Response.type';
+
+export class ParseHalJsonInterceptor<T>
+  implements NestInterceptor<T, RestResponseCreateUser<T>>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<T>,
+  ):
+    | Observable<RestResponseCreateUser<T>>
+    | Promise<Observable<RestResponseCreateUser<T>>> {
+    return next.handle().pipe(
+      map((data) => {
+        return {
+          _links: {
+            self: {
+              href: '/users',
+            },
+          },
+          data,
+        };
+      }),
+    );
+  }
+}
