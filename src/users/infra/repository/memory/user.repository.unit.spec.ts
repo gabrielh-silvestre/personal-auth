fe2ad5f3704334595f586c5f4a89512fde4a63e2
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-07 15:46:07 -0300

    feat(shared/utils): create central mock
    - substitute local mocks

--- src/users/infra/repository/memory/user.repository.unit.spec.ts
+++ src/users/infra/repository/memory/user.repository.unit.spec.ts
@@ -3,11 +3,7 @@ import { v4 as uuid } from 'uuid';
 import { User } from '@users/domain/entity/User';
 import { UserInMemoryRepository } from './User.repository';
 
-const USERS_MOCK: User[] = [
-  new User(uuid(), 'John', 'john@email.com'),
-  new User(uuid(), 'Doe', 'doe@email.com'),
-  new User(uuid(), 'Jane', 'jane@email.com'),
-];
+import { USERS_MOCK } from '@shared/utils/mocks/users.mock';
 
 describe('Unit test infra in memory User repository', () => {
   beforeEach(() => {
