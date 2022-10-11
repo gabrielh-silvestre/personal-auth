95bf24bb3688f8dbfbadb855d8a78bf97cd06af2
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-11 09:53:22 -0300

    feat(users/infra/grpc): create login and recover user grpc sevices
    - implement new cases on controller
    - cover new cases with integration tests

--- src/users/infra/grpc/server/controller/User.grpc-server.controller.ts
+++ src/users/infra/grpc/server/controller/User.grpc-server.controller.ts
@@ -1,22 +1,56 @@
-import { Controller, UseFilters } from '@nestjs/common';
+import { Controller, UseFilters, UseGuards, UsePipes } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 
 import type {
   InputCreateUserDto,
   OutputCreateUserDto,
-} from 'src/users/dto/CreateUser.dto';
+} from '@users/dto/CreateUser.dto';
+import type {
+  InputLoginUserDto,
+  OutputLoginUserDto,
+} from '@users/dto/LoginUser.dto';
+import type { OutputRecoverUserDto } from '@users/dto/RecoverUser.dto';
 
+import { TokenService } from '@tokens/token.service';
 import { UserService } from '@users/user.service';
+
+import { ValidateUserGuard } from '@auth/infra/guard/ValidateUser.guard';
+import { ValidateTokenGuard } from '@auth/infra/guard/ValidateToken.guard';
+
+import { ExceptionFactory } from '@exceptions/factory/Exception.factory';
 import { ExceptionFilterRpc } from '../filter/ExceptionFilter.grpc';
 
 @Controller()
 @UseFilters(new ExceptionFilterRpc())
 export class UserGrpcServerController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly tokenService: TokenService,
+  ) {}
 
   @GrpcMethod('UserService')
   async createUser(data: InputCreateUserDto): Promise<OutputCreateUserDto> {
     const { id, username } = await this.userService.create(data);
     return { id, username };
+  }
+
+  @UseGuards(ValidateUserGuard)
+  @GrpcMethod('UserService')
+  async loginUser(data: InputLoginUserDto): Promise<OutputLoginUserDto> {
+    const foundUser = await this.userService.findByEmail(data.email);
+
+    if (!foundUser || !foundUser.password.isEqual(data.password)) {
+      throw ExceptionFactory.unauthorized('Invalid credentials');
+    }
+
+    return this.tokenService.createToken({ userId: foundUser.id });
+  }
+
+  @UseGuards(ValidateTokenGuard)
+  @GrpcMethod('UserService')
+  async recoverUser(data: { token: string }): Promise<OutputRecoverUserDto> {
+    const { userId } = await this.tokenService.recoverTokenPayload(data.token);
+
+    return this.userService.getUser(userId);
   }
 }
--- src/users/infra/grpc/server/controller/user.grpc-server.controller.integration.spec.ts
+++ src/users/infra/grpc/server/controller/user.grpc-server.controller.integration.spec.ts
@@ -4,7 +4,24 @@ import { UserGrpcServerController } from './User.grpc-server.controller';
 import { UserService } from '@users/user.service';
 import { UserInMemoryRepository } from '@users/infra/repository/memory/User.repository';
 
+import { TokenService } from '@tokens/token.service';
+import { TokenInMemoryRepository } from '@tokens/infra/repository/memory/Token.repository';
+
 import { USERS_MOCK } from '@shared/utils/mocks/users.mock';
+import { JwtModule } from '@nestjs/jwt';
+
+const VALID_NEW_USER = {
+  username: 'Joe',
+  email: 'joe@email.com',
+  confirmEmail: 'joe@email.com',
+  password: 'password',
+  confirmPassword: 'password',
+};
+
+const VALID_LOGIN_USER = {
+  email: VALID_NEW_USER.email,
+  password: VALID_NEW_USER.password,
+};
 
 describe('Integration test for gRPC server User controller', () => {
   let userController: UserGrpcServerController;
@@ -13,12 +30,24 @@ describe('Integration test for gRPC server User controller', () => {
     UserInMemoryRepository.reset(USERS_MOCK);
 
     const module = await Test.createTestingModule({
+      imports: [
+        JwtModule.register({
+          secret: 'secret',
+          verifyOptions: { maxAge: '1m' },
+          signOptions: { expiresIn: '1m' },
+        }),
+      ],
       controllers: [UserGrpcServerController],
       providers: [
         UserService,
+        TokenService,
         {
           provide: 'USER_REPO',
           useClass: UserInMemoryRepository,
+        },
+        {
+          provide: 'TOKEN_REPO',
+          useClass: TokenInMemoryRepository,
         },
       ],
     }).compile();
@@ -30,17 +59,40 @@ describe('Integration test for gRPC server User controller', () => {
 
   describe('create', () => {
     it('should create a user', async () => {
-      const newUser = await userController.createUser({
-        username: 'Joe',
-        email: 'joe@email.com',
-        confirmEmail: 'joe@email.com',
-        password: 'password',
-        confirmPassword: 'password',
-      });
+      const newUser = await userController.createUser(VALID_NEW_USER);
 
       expect(newUser).not.toBeNull();
-      expect(newUser.id).toBeDefined();
-      expect(newUser.username).toBe('Joe');
+      expect(newUser).toStrictEqual({
+        id: expect.any(String),
+        username: expect.any(String),
+      });
+    });
+  });
+
+  describe('login', () => {
+    it('should login a user', async () => {
+      await userController.createUser(VALID_NEW_USER);
+      const login = await userController.loginUser(VALID_LOGIN_USER);
+
+      expect(login).not.toBeNull();
+      expect(login).toStrictEqual({
+        token: expect.any(String),
+      });
+    });
+  });
+
+  describe('recover user', () => {
+    it('should recover a user', async () => {
+      await userController.createUser(VALID_NEW_USER);
+      const { token } = await userController.loginUser(VALID_LOGIN_USER);
+
+      const recover = await userController.recoverUser({ token });
+
+      expect(recover).not.toBeNull();
+      expect(recover).toStrictEqual({
+        id: expect.any(String),
+        username: expect.any(String),
+      });
     });
   });
 });
