ac14acc9e0a42d1cdff00bd9114abc5f9c42975f
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-14 15:11:01 -0300

    feat(mail): create service for mail
    - instantiate and export mail service

---
+++ src/mail/mail.service.ts
@@ -0,0 +1,21 @@
+import { Inject, Injectable } from '@nestjs/common/decorators';
+import { Transporter } from 'nodemailer';
+
+import { Mail } from './domain/entity/Mail';
+
+@Injectable()
+export class MailService {
+  constructor(
+    @Inject('MAILER_SERVICE') private readonly mailerService: Transporter,
+  ) {}
+
+  async sendMail(mail: Mail) {
+    this.mailerService.sendMail({
+      to: mail.to,
+      subject: mail.subject,
+      text: mail.text,
+      html: mail.html,
+      cc: mail.cc,
+    });
+  }
+}
