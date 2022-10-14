710b4f2874565effef82ec8e59b59d9fdd0a8982
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-14 15:12:40 -0300

    refactor(app.module): remove unused modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common/decorators';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 
 import { UserModule } from '@users/user.module';
-import { TokenModule } from '@tokens/token.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -15,7 +14,6 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/nestjs';
     MongooseModule.forRoot(MONGO_URI),
     EventEmitterModule.forRoot(),
     UserModule,
-    TokenModule,
   ],
   controllers: [AppController],
   providers: [AppService],
