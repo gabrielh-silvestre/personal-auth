67d48708bb2dc4f31f0f5fb75b319821053f72db
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-20 19:03:34 -0300

    feat: inject Token and Auth module on root module
    - change proto files path

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
 
 import { UserModule } from '@users/user.module';
 
+import { TokenModule } from '@tokens/token.module';
+import { AuthModule } from '@auth/auth.module';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -14,6 +17,8 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/nestjs';
     MongooseModule.forRoot(MONGO_URI),
     EventEmitterModule.forRoot(),
     UserModule,
+    TokenModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
