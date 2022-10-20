4fa6bcea4728fc86afc991a24033f58e4018fad8
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-20 18:58:43 -0300

    refactor(tokens/infra): move proto to /infra root
    - export and init use cases

--- src/tokens/token.module.ts
+++ src/tokens/token.module.ts
@@ -3,7 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { JwtModule } from '@nestjs/jwt';
 
 import { RevokeTokenController } from './infra/api/controller/RevokeToken.controller';
+
 import { RevokeTokenUseCase } from './useCase/revoke/RevokeToken.useCase';
+import { CreateTokenUseCase } from './useCase/create/CreateToken.useCase';
+import { ValidateTokenUseCase } from './useCase/validate/ValidateToken.useCase';
 
 import { TokenMongooseRepository } from './infra/repository/mongoose/Token.repository';
 import {
@@ -28,11 +31,13 @@ const EXPIRES_IN = process.env.JWT_EXPIRES_IN || '1d';
   controllers: [RevokeTokenController],
   providers: [
     RevokeTokenUseCase,
+    CreateTokenUseCase,
+    ValidateTokenUseCase,
     {
       provide: 'TOKEN_REPO',
       useClass: TokenMongooseRepository,
     },
   ],
-  exports: [],
+  exports: [CreateTokenUseCase, ValidateTokenUseCase],
 })
 export class TokenModule {}
