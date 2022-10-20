71cea86ae25bd966ce5d4c7e2f517bb413c8fd05
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-20 20:48:22 -0300

    test: ignore @types and main.ts from coverage

--- test/jest-all.config.js
+++ test/jest-all.config.js
@@ -4,4 +4,5 @@ module.exports = {
   ...mainConfig,
   testRegex: '.*/(test|src)/.*/*.(spec|e2e-spec).ts$',
   rootDir: '../',
+  modulePathIgnorePatterns: ['<rootDir>/src/@types', '<rootDir>/src/main.ts'],
 };
