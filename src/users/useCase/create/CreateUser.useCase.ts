6f49d928a95ea608e98b189490a7635b42399d07
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-24 10:09:12 -0300

    feat(users/useCase/create): emit user.created event

--- src/users/useCase/create/CreateUser.useCase.ts
+++ src/users/useCase/create/CreateUser.useCase.ts
@@ -1,19 +1,29 @@
-import { ExceptionFactory } from '@exceptions/factory/Exception.factory';
 import { Inject, Injectable } from '@nestjs/common';
+import { EventEmitter2 } from '@nestjs/event-emitter';
 
 import type { IUserRepository } from '@users/domain/repository/user.repository.interface';
 import type { InputCreateUserDto, OutputCreateUserDto } from './CreateUser.dto';
+import type { IUser } from '@users/domain/entity/user.interface';
 
 import { UserFactory } from '@users/domain/factory/User.factory';
+import { EventFactory } from '@shared/modules/event/factory/Event.factory';
+import { ExceptionFactory } from '@exceptions/factory/Exception.factory';
 
 @Injectable()
 export class CreateUserUseCase {
   constructor(
     @Inject('USER_REPO') private readonly userRepository: IUserRepository,
+    private readonly eventEmitter: EventEmitter2,
   ) {}
 
   private async isEmailAlreadyInUse(email: string): Promise<boolean> {
     return this.userRepository.existsByEmail(email);
+  }
+
+  private async createUserEvent(user: IUser): Promise<void | never> {
+    const event = EventFactory.create<IUser>('user.created', user);
+
+    this.eventEmitter.emit(event.name, event);
   }
 
   async execute({
@@ -38,6 +48,7 @@ export class CreateUserUseCase {
     }
 
     await this.userRepository.create(newUser);
+    this.createUserEvent(newUser);
 
     return {
       id: newUser.id,
