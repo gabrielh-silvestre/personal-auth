d1b8b3503f21506a3e72c69efeaaab71124accd2
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-24 18:26:24 -0300

    feat(tokens/domain/factory): add case recover password token
    - cover new case with unit tests

--- src/tokens/domain/factory/token.factory.unit.spec.ts
+++ src/tokens/domain/factory/token.factory.unit.spec.ts
@@ -1,10 +1,11 @@
+import { TokenType } from '../entity/token.interface';
 import { TokenFactory } from './Token.factory';
 
 const VALID_USER_ID = '5f4d2e2e-2b9a-4da3-9d5b-1b8e7b3dcb6d';
 
 describe('Test domain Token factory', () => {
-  it('should create a new token', () => {
-    const token = TokenFactory.create(VALID_USER_ID);
+  it('should create a new access token', () => {
+    const token = TokenFactory.createAccessToken(VALID_USER_ID);
 
     expect(token).toBeDefined();
 
@@ -16,5 +17,22 @@ describe('Test domain Token factory', () => {
     expect(token.revoked).toBeFalsy();
 
     expect(token.lastRefresh < token.expires).toBeTruthy();
+    expect(token.type).toBe(TokenType.ACCESS);
+  });
+
+  it('should create a new recover password token', () => {
+    const token = TokenFactory.createRecoverPasswordToken(VALID_USER_ID);
+
+    expect(token).toBeDefined();
+
+    expect(token.id).toBeDefined();
+    expect(token.userId).toBeDefined();
+    expect(token.lastRefresh).toBeDefined();
+
+    expect(token.expires).toBeDefined();
+    expect(token.revoked).toBeFalsy();
+
+    expect(token.lastRefresh < token.expires).toBeTruthy();
+    expect(token.type).toBe(TokenType.RECOVER_PASSWORD);
   });
 });
