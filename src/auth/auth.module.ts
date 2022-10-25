0c5b499819a72312f16a22ce5e364bff5b9da02b
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-25 10:40:50 -0300

    feat(auth): inject ForgotPassword features on auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,17 +3,22 @@ import { forwardRef, Module } from '@nestjs/common';
 import { TokenModule } from '@tokens/token.module';
 import { UserModule } from '@users/user.module';
 
-import { LoginController } from './infra/api/controller/Login.controller';
+import { LoginController } from './infra/api/controller/login/Login.controller';
 import { LoginUseCase } from './useCase/login/Login.useCase';
+
+import { ForgotPasswordController } from './infra/api/controller/forgotPassword/ForgotPassword.controller';
+import { ForgotPasswordUseCase } from './useCase/forgotPassword/ForgotPassword.useCase';
 
 import { TokenServiceAdaptor } from './infra/service/token/Token.service.adaptor';
 import { UserServiceAdaptor } from './infra/service/user/User.service.adaptor';
+import { MailServiceAdaptor } from './infra/service/mail/Mail.service.adaptor';
 
 @Module({
   imports: [forwardRef(() => UserModule), TokenModule],
-  controllers: [LoginController],
+  controllers: [LoginController, ForgotPasswordController],
   providers: [
     LoginUseCase,
+    ForgotPasswordUseCase,
     {
       provide: 'TOKEN_SERVICE',
       useClass: TokenServiceAdaptor,
@@ -22,6 +27,10 @@ import { UserServiceAdaptor } from './infra/service/user/User.service.adaptor';
       provide: 'USER_SERVICE',
       useClass: UserServiceAdaptor,
     },
+    {
+      provide: 'MAIL_SERVICE',
+      useClass: MailServiceAdaptor,
+    }
   ],
   exports: [{ provide: 'TOKEN_SERVICE', useClass: TokenServiceAdaptor }],
 })
