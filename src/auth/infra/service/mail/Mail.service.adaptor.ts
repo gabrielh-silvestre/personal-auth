c3abebbd0ae3107b1d033302468832396929fde9
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-28 14:20:09 -0300

    fix(**/service/mail): build object before emit event

--- src/auth/infra/service/mail/Mail.service.adaptor.ts
+++ src/auth/infra/service/mail/Mail.service.adaptor.ts
@@ -6,19 +6,31 @@ import type {
   InputRecoverPasswordMail,
 } from './mail.service.interface';
 
+type MailDataDto = {
+  to: string;
+  subject: string;
+  text: string;
+  html: string;
+};
+
 @Injectable()
 export class MailServiceAdaptor implements IMailService {
   constructor(@Inject('mail_queue') private readonly client: ClientProxy) {}
 
-  async recoverPasswordMail({
+  private buildMailData({
     email,
     username,
     token,
-  }: InputRecoverPasswordMail): Promise<void> {
-    this.client.emit('send_reset_password_mail', {
-      email,
-      username,
-      token,
-    });
+  }: InputRecoverPasswordMail): MailDataDto {
+    return {
+      to: email,
+      subject: 'Reset your password',
+      text: `Hi ${username}, use this token to reset your password: ${token}`,
+      html: `<b>Hi ${username}, use this token to reset your password: ${token}</b>`,
+    };
+  }
+
+  async recoverPasswordMail(data: InputRecoverPasswordMail): Promise<void> {
+    this.client.emit('send_reset_password_mail', this.buildMailData(data));
   }
 }
--- src/users/infra/service/mail/Mail.service.adaptor.ts
+++ src/users/infra/service/mail/Mail.service.adaptor.ts
@@ -3,14 +3,27 @@ import { ClientProxy } from '@nestjs/microservices';
 
 import type { IMailService, InputWelcomeMail } from './mail.service.interface';
 
+type MailDataDto = {
+  to: string;
+  subject: string;
+  text: string;
+  html: string;
+};
+
 @Injectable()
 export class MailServiceAdaptor implements IMailService {
   constructor(@Inject('mail_queue') private readonly client: ClientProxy) {}
 
-  async welcomeMail({ email, username }: InputWelcomeMail): Promise<void> {
-    this.client.emit('send_welcome_mail', {
-      email,
-      username,
-    });
+  private buildMailData({ email, username }: InputWelcomeMail): MailDataDto {
+    return {
+      to: email,
+      subject: 'Welcome to the S1 auth service!',
+      text: `Welcome to the S1 auth service, ${username}!`,
+      html: `<b>Welcome to the S1 auth service, ${username}!</b>`,
+    };
+  }
+
+  async welcomeMail(data: InputWelcomeMail): Promise<void> {
+    this.client.emit('send_welcome_mail', this.buildMailData(data));
   }
 }
