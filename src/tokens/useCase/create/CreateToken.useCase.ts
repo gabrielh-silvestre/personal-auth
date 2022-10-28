d60cbf13a6bc687d1520883ee771a294fa9611de
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-28 16:59:40 -0300

    refactor(tokens/useCase/create): remove jwt encrypt responsability
    - addapt test

--- src/tokens/useCase/create/CreateToken.useCase.ts
+++ src/tokens/useCase/create/CreateToken.useCase.ts
@@ -1,8 +1,8 @@
 import { Inject, Injectable } from '@nestjs/common';
 
 import type { ITokenRepository } from '@tokens/domain/repository/token.repository.interface';
-import type { IJwtService } from '@tokens/infra/service/jwt/Jwt.service.interface';
-import type { CreateTokenPayload, TokenTypeDto } from './CreateToken.dto';
+import type { TokenTypeDto } from './CreateToken.dto';
+import type { IToken } from '@tokens/domain/entity/token.interface';
 
 import { TokenFactory } from '@tokens/domain/factory/Token.factory';
 
@@ -10,23 +10,16 @@ import { TokenFactory } from '@tokens/domain/factory/Token.factory';
 export class CreateTokenUseCase {
   constructor(
     @Inject('TOKEN_REPO') private readonly tokenRepository: ITokenRepository,
-    @Inject('JWT_SERVICE')
-    private readonly jwtService: IJwtService<CreateTokenPayload>,
   ) {}
 
-  async execute(id: string, type: TokenTypeDto): Promise<string> {
+  async execute(id: string, type: TokenTypeDto): Promise<IToken> {
     const newToken =
       type === 'ACCESS'
         ? TokenFactory.createAccessToken(id)
         : TokenFactory.createRecoverPasswordToken(id);
 
-    const jwtToken = await this.jwtService.encrypt({
-      tokenId: newToken.id,
-      userId: newToken.userId,
-    });
-
     this.tokenRepository.create(newToken);
 
-    return jwtToken;
+    return newToken;
   }
 }
