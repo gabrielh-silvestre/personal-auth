2076af1a8a261c4e461a96c657dd7ea79b3e2b27
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-29 17:15:00 -0300

    refactor(shared/modules/rmq): recover Rmq config from env var
    - create constants for new vars

--- src/shared/utils/constants/envVariables.ts
+++ src/shared/utils/constants/envVariables.ts
@@ -1,3 +1,8 @@
 export const MONGO_URI = 'MONGO_URI';
+
 export const JWT_SECRET = 'JWT_SECRET';
 export const JWT_EXPIRES_IN = 'JWT_EXPIRES_IN';
+
+export const RABBITMQ_URL = 'RABBITMQ_URL';
+
+export const RABBITMQ_QUEUE = (name: string) => `RABBITMQ_${name}_QUEUE`;
