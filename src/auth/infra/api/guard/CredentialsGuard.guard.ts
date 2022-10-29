5da8259b0c7ab1a791b51caa5b06003770c446da
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-29 17:54:20 -0300

    fix(auth/infra/api): convert credentials from grpc to http body

--- src/auth/infra/api/guard/CredentialsGuard.guard.ts
+++ src/auth/infra/api/guard/CredentialsGuard.guard.ts
@@ -1,5 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
+
+import type { TokenPayload } from '@auth/infra/service/token/Token.service.adaptor';
+import type { InputLoginDto } from '@auth/useCase/login/Login.dto';
+
+import { ExceptionFactory } from '@exceptions/factory/Exception.factory';
 
 @Injectable()
-export class CredentialsGuard extends AuthGuard('local') {}
+export class CredentialsGuard extends AuthGuard('local') {
+  private convertGrpcCredentialsToHttpBody(context: ExecutionContext): void {
+    const { email, password } = context.switchToRpc().getData<InputLoginDto>();
+    context.switchToHttp().getRequest().body = { email, password };
+  }
+
+  handleRequest<T = TokenPayload>(err: any, user: any): T {
+    if (err || !user) {
+      throw ExceptionFactory.forbidden('Invalid credentials');
+    }
+
+    return user;
+  }
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const isGrpcRequest = context.getType() === 'rpc';
+
+    if (isGrpcRequest) {
+      this.convertGrpcCredentialsToHttpBody(context);
+    }
+
+    return super.canActivate(context);
+  }
+}
