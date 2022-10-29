1ebe7039e5f6bbe2748b7a3b90d2fa4e5b2d86aa
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-29 14:44:46 -0300

    feat(auth/infra): create new authentication strategy
    - use jwt passport for token validation
    - inject jwt module with config

---
+++ src/auth/infra/strategy/Jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+import type { TokenPayload } from '../service/token/Token.service.adaptor';
+import type { ITokenService } from '../service/token/token.service.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @Inject('TOKEN_SERVICE') private readonly tokenService: ITokenService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET || 'secret',
+    });
+  }
+
+  async validate(payload: TokenPayload): Promise<TokenPayload> {
+    await this.tokenService.verifyToken(payload.tokenId);
+
+    return { userId: payload.userId, tokenId: payload.tokenId };
+  }
+}
