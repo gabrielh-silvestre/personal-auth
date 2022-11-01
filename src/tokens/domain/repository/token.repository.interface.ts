7f875d2f00b7bc4bff1c9fad1e4bf9522dfb9377
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-11-01 18:00:45 -0300

    feat(tokens/domain/repository): add findByUserIdAndType method signature

--- src/tokens/domain/repository/token.repository.interface.ts
+++ src/tokens/domain/repository/token.repository.interface.ts
@@ -1,5 +1,8 @@
 import type { IRepository } from '@shared/domain/repository/repository.interface';
 
 import { Token } from '../entity/Token';
+import { TokenType } from '../entity/token.interface';
 
-export type ITokenRepository = Omit<IRepository<Token>, 'findAll'>;
+export interface ITokenRepository extends Omit<IRepository<Token>, 'findAll'> {
+  findByUserIdAndType(userId: string, type: TokenType): Promise<Token | null>;
+}
