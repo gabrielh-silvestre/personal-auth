cd6161531df534cf490ec277b545859c87a5b5ea
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-11-03 23:27:38 -0300

    refactor(auth/infra/service): stop encrypt just return TokenPayload
    - addapt interface

--- src/auth/infra/service/token/Token.service.adaptor.ts
+++ src/auth/infra/service/token/Token.service.adaptor.ts
@@ -1,16 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 
-import type { ITokenService } from './token.service.interface';
+import type { ITokenService, TokenPayload } from './token.service.interface';
 
 import { CreateTokenUseCase } from '@tokens/useCase/create/CreateToken.useCase';
 import { ValidateTokenUseCase } from '@tokens/useCase/validate/ValidateToken.useCase';
 import { RefreshTokenUseCase } from '@tokens/useCase/refresh/RefreshToken.useCase';
-
-export type TokenPayload = {
-  userId: string;
-  tokenId: string;
-};
 
 @Injectable()
 export class TokenServiceAdaptor implements ITokenService {
@@ -18,49 +12,50 @@ export class TokenServiceAdaptor implements ITokenService {
     private readonly createTokenUseCase: CreateTokenUseCase,
     private readonly refreshTokenUseCase: RefreshTokenUseCase,
     private readonly validateTokenUseCase: ValidateTokenUseCase,
-    private readonly jwtService: JwtService,
   ) {}
 
-  async generateAccessToken(userId: string): Promise<string> {
+  async generateAccessToken(userId: string): Promise<TokenPayload | never> {
     const token = await this.createTokenUseCase.execute(userId, 'ACCESS');
 
-    return this.jwtService.signAsync({
+    return {
       userId: token.userId,
       tokenId: token.id,
-    });
+    };
   }
 
-  async generateRecoverPasswordToken(userId: string): Promise<string> {
+  async generateRecoverPasswordToken(
+    userId: string,
+  ): Promise<TokenPayload | never> {
     const token = await this.createTokenUseCase.execute(
       userId,
       'RECOVER_PASSWORD',
     );
 
-    return this.jwtService.signAsync({
+    return {
       userId: token.userId,
       tokenId: token.id,
-    });
+    };
   }
 
-  async generateRefreshToken(userId: string): Promise<string> {
+  async generateRefreshToken(userId: string): Promise<TokenPayload | never> {
     const token = await this.createTokenUseCase.execute(userId, 'REFRESH');
 
-    return this.jwtService.signAsync({
+    return {
       userId: token.userId,
       tokenId: token.id,
-    });
+    };
   }
 
-  async refreshToken(tokenId: string): Promise<string> {
+  async refreshToken(tokenId: string): Promise<TokenPayload | never> {
     const token = await this.refreshTokenUseCase.execute(tokenId);
 
-    return this.jwtService.signAsync({
+    return {
       userId: token.userId,
       tokenId: token.tokenId,
-    });
+    };
   }
 
-  async verifyToken(tokenId: string): Promise<TokenPayload> {
+  async verifyToken(tokenId: string): Promise<TokenPayload | never> {
     return this.validateTokenUseCase.execute(tokenId);
   }
 }
--- src/auth/infra/service/token/token.service.interface.ts
+++ src/auth/infra/service/token/token.service.interface.ts
@@ -1,11 +1,11 @@
-export type OutputCreateToken = {
-  tokenId: string;
+export type TokenPayload = {
   userId: string;
+  tokenId: string;
 };
 
 export interface ITokenService {
-  generateAccessToken(userId: string): Promise<string>;
-  generateRecoverPasswordToken(userId: string): Promise<string>;
-  generateRefreshToken(userId: string): Promise<string>;
-  verifyToken(token: string): Promise<OutputCreateToken | never>;
+  generateAccessToken(userId: string): Promise<TokenPayload | never>;
+  generateRecoverPasswordToken(userId: string): Promise<TokenPayload | never>;
+  generateRefreshToken(userId: string): Promise<TokenPayload | never>;
+  verifyToken(token: string): Promise<TokenPayload | never>;
 }
