e13552f87e4e7e64b673bc42df427698b132812a
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-11-14 19:53:15 -0300

    feat(users/infra/facade): implement facade pattern
    - remove direct access to useCase layer
    - only exports facade

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common/decorators';
-import { ConfigModule } from '@nestjs/config';
+// import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from '@auth/auth.module';
 import { RmqModule } from '@shared/modules/rmq/rmq.module';
@@ -9,6 +9,8 @@ import { UserPrismaRepository } from './infra/repository/prisma/User.repository'
 import { CreateUserController } from './infra/api/controller/create/CreateUser.controller';
 import { GetMeController } from './infra/api/controller/getMe/GetMe.controller';
 
+import { UserFacade } from './infra/facade/User.facade';
+
 import { CreateUserUseCase } from './useCase/create/CreateUser.useCase';
 import { GetUserByIdUseCase } from './useCase/getById/GetUserById.useCase';
 import { GetUserByEmailUseCase } from './useCase/getByEmail/GetUserByEmail.useCase';
@@ -17,12 +19,12 @@ import { MailServiceAdaptor } from './infra/service/mail/Mail.service.adaptor';
 
 @Module({
   imports: [RmqModule.register('MAIL'), AuthModule],
-  exports: [GetUserByIdUseCase, GetUserByEmailUseCase],
   controllers: [CreateUserController, GetMeController],
   providers: [
     CreateUserUseCase,
     GetUserByIdUseCase,
     GetUserByEmailUseCase,
+    UserFacade,
     {
       provide: 'MAIL_SERVICE',
       useClass: MailServiceAdaptor,
@@ -32,5 +34,6 @@ import { MailServiceAdaptor } from './infra/service/mail/Mail.service.adaptor';
       useClass: UserPrismaRepository,
     },
   ],
+  exports: [UserFacade],
 })
 export class UserModule {}
