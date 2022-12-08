f84a8b8ee4e417d3d0d350e1f5ff064108c8774c
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-12-08 18:13:24 -0300

    feat(auth/infra/service): create method for validate jwt encripted token

--- src/auth/infra/service/token/Token.service.adaptor.ts
+++ src/auth/infra/service/token/Token.service.adaptor.ts
@@ -6,12 +6,15 @@ import { CreateTokenUseCase } from '@tokens/useCase/create/CreateToken.useCase';
 import { ValidateTokenUseCase } from '@tokens/useCase/validate/ValidateToken.useCase';
 import { RefreshTokenUseCase } from '@tokens/useCase/refresh/RefreshToken.useCase';
 
+import { JwtAccessService } from '@shared/modules/jwt/JwtAccess.service';
+
 @Injectable()
 export class TokenServiceAdaptor implements ITokenService {
   constructor(
     private readonly createTokenUseCase: CreateTokenUseCase,
     private readonly refreshTokenUseCase: RefreshTokenUseCase,
     private readonly validateTokenUseCase: ValidateTokenUseCase,
+    private readonly jwtAccessService: JwtAccessService,
   ) {}
 
   async generateAccessToken(userId: string): Promise<string | never> {
@@ -40,5 +43,10 @@ export class TokenServiceAdaptor implements ITokenService {
 
   async verifyToken(tokenId: string): Promise<TokenPayload | never> {
     return this.validateTokenUseCase.execute(tokenId);
+  }
+
+  async verifyJwtToken(token: string): Promise<TokenPayload> {
+    const { tokenId } = await this.jwtAccessService.verify<TokenPayload>(token);
+    return this.verifyToken(tokenId);
   }
 }
