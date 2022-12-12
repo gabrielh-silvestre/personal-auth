1e58fe08265148c13aa72d20e9dd21df9702b28e
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-12-12 20:49:10 -0300

    fix(main): comment user proto file

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
       url: GRPC_URL,
       package: ['proto.users', 'proto.tokens', 'proto.auth'],
       protoPath: [
-        join(__dirname, '../users/infra/proto/user.proto'),
+        // join(__dirname, '../users/infra/proto/user.proto'),
         join(__dirname, '../tokens/infra/proto/token.proto'),
         join(__dirname, '../auth/infra/proto/auth.proto'),
       ],
