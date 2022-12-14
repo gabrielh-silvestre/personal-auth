72e141d0195f29a61f627d40448ac809aa8a9490
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-12-14 18:35:39 -0300

    refactor(auth/infra/api): remove manual ack from VerifyTokenController
    - remove internal token verification from JwtAccessTokenStrategy

--- src/auth/infra/api/controller/verifyToken/VerifyToken.controller.ts
+++ src/auth/infra/api/controller/verifyToken/VerifyToken.controller.ts
@@ -1,48 +1,29 @@
-import { Controller, UseFilters, UseGuards } from '@nestjs/common';
-import {
-  Ctx,
-  MessagePattern,
-  Payload,
-  RmqContext,
-} from '@nestjs/microservices';
+import type { Request } from 'express';
 
-import type {
-  InputVerifyTokenDto,
-  OutputVerifyTokenDto,
-} from '@auth/useCase/verifyToken/VerifyToken.dto';
+import { Controller, UseFilters, UseGuards } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+
+import type { OutputVerifyTokenDto } from '@auth/useCase/verifyToken/VerifyToken.dto';
 
 import { VerifyTokenUseCase } from '@auth/useCase/verifyToken/VerifyToken.useCase';
-
-import { RmqService } from '@shared/modules/rmq/rmq.service';
 
 import { AuthenticateGuard } from '../../guard/Authenticate.guard';
 import { ExceptionFilterRpc } from '@shared/infra/filter/ExceptionFilter.grpc';
 
 @Controller()
 export class VerifyTokenController {
-  constructor(
-    private readonly verifyTokenUseCase: VerifyTokenUseCase,
-    private readonly rmqService: RmqService,
-  ) {}
+  constructor(private readonly verifyTokenUseCase: VerifyTokenUseCase) {}
 
   @UseGuards(AuthenticateGuard)
   @UseFilters(new ExceptionFilterRpc())
   @MessagePattern('auth.verify_token')
   async handle(
-    @Payload() data: InputVerifyTokenDto,
-    @Ctx() ctx: RmqContext,
+    @Payload() data: Request,
   ): Promise<OutputVerifyTokenDto | never> {
-    console.log(data);
-    try {
-      const { userId } = await this.verifyTokenUseCase.execute(data);
+    const { userId } = await this.verifyTokenUseCase.execute({
+      tokenId: data.user.tokenId,
+    });
 
-      this.rmqService.ack(ctx);
-
-      return { userId };
-    } catch (err) {
-      this.rmqService.nack(ctx);
-
-      throw err;
-    }
+    return { userId };
   }
 }
