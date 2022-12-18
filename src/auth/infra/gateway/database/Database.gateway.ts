de0e09717af643ed6d73a362f29bcd6c6ed9e0b0
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-12-18 15:46:35 -0300

    feat(auth/infra/gateway): create gateway for database
    - implement following ITokenRepository interface

---
+++ src/auth/infra/gateway/database/Database.gateway.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+
+import type { TokenType } from '@auth/domain/entity/token.interface';
+import type { IDatabaseGateway } from './Database.gateway.interface';
+import type { IDatabaseAdapter } from '@auth/infra/adapter/database/Database.adapter.interface';
+
+import { Token } from '@auth/domain/entity/Token';
+
+@Injectable()
+export class DatabaseGateway implements IDatabaseGateway {
+  constructor(private readonly databaseAdapter: IDatabaseAdapter) {}
+
+  async find(id: string): Promise<Token> {
+    return this.databaseAdapter.findOne({ id });
+  }
+
+  async findByUserIdAndType(
+    userId: string,
+    type: TokenType,
+  ): Promise<Token | null> {
+    return this.databaseAdapter.findOne({
+      userId,
+      type,
+    });
+  }
+
+  async create(entity: Token): Promise<void> {
+    await this.databaseAdapter.create(entity);
+  }
+
+  async update(entity: Token): Promise<void> {
+    await this.databaseAdapter.update(entity);
+  }
+}
