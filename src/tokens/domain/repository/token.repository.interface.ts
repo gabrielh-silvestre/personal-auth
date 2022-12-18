f89fa90cdc7417e6dab702eb8c7e28da0bd914ea
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-12-18 12:53:09 -0300

    chore(tokens): delete token module

--- src/tokens/domain/repository/token.repository.interface.ts
+++
@@ -1,8 +0,0 @@
-import type { IRepository } from '@shared/domain/repository/repository.interface';
-
-import { Token } from '../entity/Token';
-import { TokenType } from '../entity/token.interface';
-
-export interface ITokenRepository extends Omit<IRepository<Token>, 'findAll'> {
-  findByUserIdAndType(userId: string, type: TokenType): Promise<Token | null>;
-}
