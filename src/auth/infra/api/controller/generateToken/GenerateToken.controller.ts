10d84cc3de326aae8ea0765a24d414933a9369c5
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-12-18 17:11:49 -0300

    feat(auth/infra/api): make jwt encrypt on controller
    - addapt tests injecting DATABASE_ADAPTER and DATABASE_GATEWAY

--- src/auth/infra/api/controller/generateToken/GenerateToken.controller.ts
+++ src/auth/infra/api/controller/generateToken/GenerateToken.controller.ts
@@ -1,19 +1,34 @@
 import { Controller, UseFilters } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 
+import type { GenerateTokenType } from '@auth/useCase/generateToken/GenerateToken.dto';
+
 import { GenerateTokenUseCase } from '@auth/useCase/generateToken/GenerateToken.useCase';
+import { JwtAccessService } from '@shared/modules/jwt/JwtAccess.service';
 
 import { ExceptionFilterRpc } from '@shared/infra/filter/ExceptionFilter.grpc';
 
 @Controller()
 export class GenerateTokenController {
-  constructor(private readonly generateTokenUseCase: GenerateTokenUseCase) {}
+  constructor(
+    private readonly generateTokenUseCase: GenerateTokenUseCase,
+    private readonly jwtAccess: JwtAccessService,
+  ) {}
+
+  private async handle(data: {
+    userId: string;
+    type: GenerateTokenType;
+  }): Promise<string> {
+    const tokenId = await this.generateTokenUseCase.execute(data);
+
+    return this.jwtAccess.sign({ tokenId, userId: data.userId });
+  }
 
   @UseFilters(new ExceptionFilterRpc())
   @MessagePattern('auth.generate_recover_token')
   async handleRecoverToken(
     @Payload() { userId }: { userId: string },
   ): Promise<string | never> {
-    return this.generateTokenUseCase.execute({ userId, type: 'recover' });
+    return this.handle({ userId, type: 'recover' });
   }
 }
