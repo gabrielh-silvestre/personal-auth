daa4b0c7f7b6752350e7589d4a80b52e50ce29d4
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-12-18 16:13:55 -0300

    refactor(auth/useCase/refresh): remove TokenGateway
    - interact direct with Token and database
    - addapt tests

--- src/auth/useCase/refresh/Refresh.useCase.ts
+++ src/auth/useCase/refresh/Refresh.useCase.ts
@@ -1,22 +1,33 @@
 import { Inject, Injectable } from '@nestjs/common';
 
 import type { InputRefreshDto, OutputRefreshDto } from './Refresh.dto';
-import type { ITokenGateway } from '@auth/infra/gateway/token/token.gateway.interface';
+import type { IDatabaseGateway } from '@auth/infra/gateway/database/Database.gateway.interface';
 
-import { TOKEN_GATEWAY } from '@auth/utils/constants';
+import { TokenFactory } from '@auth/domain/factory/Token.factory';
+
+import { DATABASE_GATEWAY } from '@auth/utils/constants';
 
 @Injectable()
 export class RefreshUseCase {
   constructor(
-    @Inject(TOKEN_GATEWAY) private readonly tokenGateway: ITokenGateway,
+    @Inject(DATABASE_GATEWAY)
+    private readonly databaseGateway: IDatabaseGateway,
   ) {}
 
+  // TODO: Implement more complete logic
   async execute({
     userId,
   }: InputRefreshDto): Promise<OutputRefreshDto | never> {
-    const accessTokenId = await this.tokenGateway.generateAccessToken(userId);
-    const refreshTokenId = await this.tokenGateway.generateRefreshToken(userId);
+    const accessToken = TokenFactory.createAccessToken(userId);
+    const refreshToken = TokenFactory.createRefreshToken(userId);
 
-    return { accessTokenId, refreshTokenId, userId };
+    await this.databaseGateway.create(accessToken);
+    await this.databaseGateway.create(refreshToken);
+
+    return {
+      accessTokenId: accessToken.id,
+      refreshTokenId: refreshToken.id,
+      userId,
+    };
   }
 }
