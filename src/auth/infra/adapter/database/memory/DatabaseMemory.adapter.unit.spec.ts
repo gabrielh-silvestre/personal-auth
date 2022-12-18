330a70574956260eacb15b020630d221d2e28d67
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-12-18 15:45:33 -0300

    feat(auth/infra/adapter): create adapter for database communication
    - create memory adapter and cover with unit tests
    - create mongoose adapter and its schema

---
+++ src/auth/infra/adapter/database/memory/DatabaseMemory.adapter.unit.spec.ts
@@ -0,0 +1,103 @@
+import { v4 as uuid } from 'uuid';
+
+import { TokenType } from '@auth/domain/entity/token.interface';
+import { TokenFactory } from '@auth/domain/factory/Token.factory';
+
+import { DatabaseMemoryAdapter } from './DatabaseMemory.adapter';
+
+import { TOKENS_MOCK } from '@shared/utils/mocks/tokens.mock';
+
+describe('Unit test infra in memory Task repository', () => {
+  beforeEach(() => {
+    DatabaseMemoryAdapter.reset(TOKENS_MOCK);
+  });
+
+  it('should find all tokens', async () => {
+    const tokenRepository = new DatabaseMemoryAdapter();
+
+    const foundTokens = await tokenRepository.findAll();
+
+    expect(foundTokens).toBeInstanceOf(Array);
+    expect(foundTokens).toHaveLength(TOKENS_MOCK.length);
+  });
+
+  it('should find a token by id', async () => {
+    const tokenRepository = new DatabaseMemoryAdapter();
+    const [tokenToFind] = TOKENS_MOCK;
+
+    const foundToken = await tokenRepository.findOne({ id: tokenToFind.id });
+
+    expect(foundToken).not.toBeNull();
+  });
+
+  it('should create a access token', async () => {
+    const tokenRepository = new DatabaseMemoryAdapter();
+    const newToken = TokenFactory.createAccessToken(uuid());
+
+    await tokenRepository.create(newToken);
+
+    const foundToken = await tokenRepository.findOne({ id: newToken.id });
+
+    expect(foundToken).not.toBeNull();
+    expect(foundToken?.id).toBeDefined();
+    expect(foundToken?.userId).toBe(newToken.userId);
+
+    expect(foundToken?.isValid()).toBeTruthy();
+    expect(foundToken?.type).toBe(TokenType.ACCESS);
+  });
+
+  it('should create a recover password token', async () => {
+    const tokenRepository = new DatabaseMemoryAdapter();
+    const newToken = TokenFactory.createRecoverPasswordToken(uuid());
+
+    await tokenRepository.create(newToken);
+
+    const foundToken = await tokenRepository.findOne({ id: newToken.id });
+
+    expect(foundToken).not.toBeNull();
+    expect(foundToken?.id).toBeDefined();
+    expect(foundToken?.userId).toBe(newToken.userId);
+
+    expect(foundToken?.isValid()).toBeTruthy();
+    expect(foundToken?.type).toBe(TokenType.RECOVER_PASSWORD);
+  });
+
+  it('should update a token or create a new one if not found', async () => {
+    const tokenRepository = new DatabaseMemoryAdapter();
+    const [tokenToUpdate] = TOKENS_MOCK;
+
+    tokenToUpdate.revoke();
+
+    await tokenRepository.update(tokenToUpdate);
+
+    const foundToken = await tokenRepository.findOne({ id: tokenToUpdate.id });
+
+    expect(foundToken).not.toBeNull();
+    expect(foundToken?.id).toBeDefined();
+    expect(foundToken?.userId).toBe(tokenToUpdate.userId);
+
+    expect(foundToken?.isValid()).toBeFalsy();
+
+    const newToken = TokenFactory.createAccessToken(uuid());
+
+    await tokenRepository.update(newToken);
+
+    const foundNewToken = await tokenRepository.findOne({ id: newToken.id });
+
+    expect(foundNewToken).not.toBeNull();
+    expect(foundNewToken?.id).toBeDefined();
+    expect(foundNewToken?.userId).toBe(newToken.userId);
+  });
+
+  it('should find a token by user id and type', async () => {
+    const tokenRepository = new DatabaseMemoryAdapter();
+    const [tokenToFind] = TOKENS_MOCK;
+
+    const foundToken = await tokenRepository.findOne({
+      id: tokenToFind.id,
+      type: tokenToFind.type,
+    });
+
+    expect(foundToken).not.toBeNull();
+  });
+});
---
+++ src/auth/infra/adapter/database/mongoose/MongooseSchema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+import { IToken, TokenType } from '@auth/domain/entity/token.interface';
+
+@Schema()
+export class TokenSchema implements IToken {
+  @Prop({ required: true, index: true, type: String, unique: true })
+  id: string;
+
+  @Prop({ required: true, index: true, type: String })
+  userId: string;
+
+  @Prop({ required: true, type: Number })
+  expireTime: number;
+
+  @Prop({ required: true, type: Date })
+  lastRefresh: Date;
+
+  @Prop({ required: true, type: Date })
+  expires: Date;
+
+  @Prop({ required: true, type: Boolean })
+  revoked: boolean;
+
+  @Prop({ required: true, type: String })
+  type: TokenType;
+}
+
+export type TokenDocument = TokenSchema & Document;
+export const tokenSchema = SchemaFactory.createForClass(TokenSchema);
