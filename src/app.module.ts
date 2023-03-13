c62fe2389f315a6da9236ed6aa33b5907d906b51
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2023-03-13 11:55:38 -0300

    chore: delete boiler plate controller and service
    - ignore strategy on tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
-import { Module } from '@nestjs/common/decorators';
+import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { AuthModule } from '@auth/auth.module';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 import { MONGO_URI } from '@shared/utils/constants';
 
@@ -25,7 +22,5 @@ import { MONGO_URI } from '@shared/utils/constants';
     }),
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
