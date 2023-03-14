7cfa68298efd797ee176d96781a44e4f8a77ccbc
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2023-03-14 16:43:30 -0300

    test(user/useCase): cover create and update password use cases with test

--- src/user/useCase/updatePassword/UpdatePassword.useCase.ts
+++ src/user/useCase/updatePassword/UpdatePassword.useCase.ts
@@ -3,18 +3,21 @@ import { Inject, Injectable } from '@nestjs/common';
 import type { IUserRepository } from '@user/domain/repository/user.repository.interface';
 import type { InputUpdatePasswordDto } from './UpdatePassword.dto';
 
+import { USER_REPOSITORY } from '@shared/utils/constants/injectNames';
+
 @Injectable()
 export class UpdatePasswordUseCase {
   constructor(
-    @Inject('USER_REPOSITORY')
+    @Inject(USER_REPOSITORY)
     private readonly userRepository: IUserRepository,
   ) {}
 
   async execute({ id, password }: InputUpdatePasswordDto): Promise<void> {
     const user = await this.userRepository.find(id);
 
-    user.password = password;
-
-    await this.userRepository.update(user);
+    if (user) {
+      user.password = password;
+      await this.userRepository.update(user);
+    }
   }
 }
